a71c6ed123ea1d218a4b1b8a3322d3843278711d
Abdus Salam <salaam.mbstu@gmail.com>
2019-07-15 22:02:35 +0600

    Bug fixed

--- Assignment-4/ProblemB.php
+++ Assignment-4/ProblemB.php
@@ -4,7 +4,7 @@ $prev = 1;
 $recent = 2;
 echo $prev.", ".$recent;
 
-for($next = 3; $next <= 40; $next = $prev + $recent) {
+for($next = 3; $next <= 1000000; $next = $prev + $recent) {
     echo ", ".$next;
     $prev = $recent;
     $recent = $next;
